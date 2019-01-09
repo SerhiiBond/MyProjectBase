1ef626b22453324c8235df07fe3d1655acd53d8d
SerhiiBond <46292623+SerhiiBond@users.noreply.github.com>
2019-01-09 07:16:09 +0200

    20180109
    L07 начата задача №1

---
+++ L07/01_Train/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01_Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ L07/01_Train/Train.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01_Train
+{
+    struct Train
+    {
+        readonly string destination;
+        readonly int numberOfTrain;
+        readonly string timeDeparture;
+
+        public Train(string destination, int numberOfTrain, string timeDeparture)
+        {
+            this.destination = destination;
+            this.numberOfTrain = numberOfTrain;
+            this.timeDeparture = timeDeparture;
+        }
+    }
+}
